3c208807f8ffdb9f4d32b93093f513fe5337ae89
Мария <marie.levchuk@gmail.com>
2020-03-01 13:28:22 +0300

    added code

--- Solution1/HW.02/Program.cs
+++ Solution1/HW.02/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace HW._02
 {
@@ -11,6 +12,25 @@ namespace HW._02
             //создание массива, который будет хранить информацию о байтах, преобразованных из строки;
             //описание цикла, который преобразовывает строковое представление числа в эквивалентное 8-битное и записывает его в массив;
             //создание файла image.png по пути C:\Temp.
+
+            StreamReader textReader = new StreamReader(@"C:\Temp\image.txt", true);
+
+            string textReaderResult = textReader.ReadToEnd();
+
+            string[] arrayOfTextResult = textReaderResult.Split(' ');
+
+            byte[] imageBytes = new byte[arrayOfTextResult.Length - 1];
+
+
+            for (int i = 0; i < arrayOfTextResult.Length - 1; i++)
+            {
+                byte binary = Convert.ToByte(arrayOfTextResult[i], 2);
+                imageBytes[i] = binary;
+            }
+
+
+            File.WriteAllBytes(@"C:\Temp\image.png", imageBytes);
+            textReader.Dispose();
         }
     }
 }
