500120e501abcfaec8662165591b49b72fdcc5d6
Мария <marie.levchuk@gmail.com>
2020-03-21 13:28:54 +0300

    Added HW05.Task2

---
+++ Solution1/HW05.Task2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace HW05.Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter symbol:");
+            char symbol = char.Parse(Console.ReadLine());
+            switch (symbol)
+            {
+                case 'w':
+                case 'W':
+                    Console.WriteLine("up");
+                    break;
+                case 'd':
+                case 'D':
+                    Console.WriteLine("right");
+                    break;
+                case 's':
+                case 'S':
+                    Console.WriteLine("down");
+                    break;
+                case 'a':
+                case 'A':
+                    Console.WriteLine("left");
+                    break;
+                default:
+                    Console.WriteLine("don't move");
+                    break;
+            }
+        }
+    }
+}
