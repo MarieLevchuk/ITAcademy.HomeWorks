21eb1b724b1dc2892a3f59a38384b929ac9d8a9d
Мария <marie.levchuk@gmail.com>
2020-03-21 14:39:43 +0300

    Added HW06.Task1

---
+++ Solution1/HW06.Task1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace HW06.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = new int[5];
+            Random rnd = new Random();
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                arr1[i] = rnd.Next(-10, 10);                
+            }
+            int[] arr2 = new int[5];
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                Console.WriteLine($"Enter a value of the element {i}:");
+                arr2[i] = int.Parse(Console.ReadLine());
+            }
+            int[] arr3 = new int[5];
+            for (int i = 0; i < arr3.Length; i++)
+            {
+                arr3[i] = arr1[i] + arr2[i];
+            }
+            Console.WriteLine("First array:");
+            foreach (var item in arr1)
+            {
+                Console.Write($"{item}\t");
+            }
+            Console.WriteLine("\nSecond array:");
+            foreach (var item in arr2)
+            {
+                Console.Write($"{item}\t");
+            }
+            Console.WriteLine("\nThird array:");
+            foreach (var item in arr3)
+            {
+                Console.Write($"{item}\t");
+            }
+        }
+    }
+}
