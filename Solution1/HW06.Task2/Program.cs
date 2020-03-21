5a80960e3c00db48f2be726c9c90aa28f1b04b54
Мария <marie.levchuk@gmail.com>
2020-03-21 15:10:37 +0300

    Added HW06.Task2

---
+++ Solution1/HW06.Task2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace HW06.Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[7];
+            for (int i = 0; i < arr.Length-1; i++)
+            {
+                Console.WriteLine($"Enter a value of the {i} element: ");
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Array: ");
+            foreach (var item in arr)
+            {
+                Console.Write($"{item}\t");
+            }
+            Console.WriteLine("\nEnter a number: ");
+            int newItem = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Enter position of new number (from 0 to {arr.Length-1}): ");
+            int newPosition = int.Parse(Console.ReadLine());
+            for (int i = arr.Length-1; i > newPosition; i--)
+            {
+                arr[i] = arr[i - 1];
+            }
+            arr[newPosition] = newItem;
+            foreach (var item in arr)
+            {
+                Console.Write($"{item}\t");
+            }
+        }
+    }
+}
