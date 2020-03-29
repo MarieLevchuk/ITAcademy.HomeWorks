174c7839d6838a716dfd24a497df8e531549803f
Мария <marie.levchuk@gmail.com>
2020-03-29 15:17:29 +0300

    Added HM.08 Task1

---
+++ Solution1/HW.08/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HW._08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите стихотворение в одну строку, разделяя строки точкой с запятой:");
+            string line = Console.ReadLine();
+            string[] arr = line.Split(';');
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i]=arr[i].Replace('О', 'А');                
+                arr[i]=arr[i].Replace('O', 'A');
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
